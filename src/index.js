d1987d8faa302c87b8729571f14ce7884cf87459
Ning Ma <ningma905@gmail.com>
2022-06-29 00:44:07 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
+const cardList = document.querySelector("ul");
+cardList.style.display = "grid";
+cardList.style.listStyle = "none";
 
-console.log(data);
+for (let i = 0; i < data.length; i++) {
+  const cardItem = document.createElement("li");
+  cardList.appendChild(cardItem);
+  cardItem.setAttribute("class", "card");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const cardTitle = document.createElement("h2");
+  cardItem.appendChild(cardTitle);
+  cardTitle.setAttribute("class", "card--title");
+  cardTitle.innerText = data[i].name;
+  cardTitle.style.textTransform = "capitalize";
+
+  const cardImg = document.createElement("img");
+  cardItem.appendChild(cardImg);
+  cardImg.setAttribute("class", "card--img");
+  cardImg.style.width = "256px";
+  cardImg.setAttribute(
+    "src",
+    data[i].sprites.other["official-artwork"].front_default
+  );
+
+  const cardTxtList = document.createElement("ul");
+  cardItem.appendChild(cardTxtList);
+  cardTxtList.setAttribute("class", "card--text");
+  cardTxtList.style.listStyle = "none";
+
+  for (let j = 0; j < data[i].stats.length; j++) {
+    const thisStats = data[i].stats;
+    const cardTxt = document.createElement("li");
+    cardTxtList.appendChild(cardTxt);
+    cardTxt.innerText = thisStats[j].stat.name + ": " + thisStats[j].base_stat;
+    cardTxt.style.textTransform = "uppercase";
+  }
+}
